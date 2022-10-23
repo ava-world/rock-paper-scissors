97a5a16fc2d56ae0ead944aa399bfd0276161c44
Avaworld <victoradetunji16@gmail.com>
2022-10-23 11:45:54 +0100

    rock paper scisirs working now

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’!
 
-    function getComputerChoice (rock, paper, scissors) {
+    function getComputerChoice () {
         var computerChoice= Math.random(); {
         if (Math.random() <= "0.33") {
             computerChoice= "rock";
@@ -18,30 +18,30 @@
 
 // a function that plays a single round of Rock Paper Scissors!
 var playerSelection = prompt("rock, paper, or scissors?");
-var computerSelection = "getComputerChoice";
+var computerSelection = getComputerChoice ();
 function playRound (playerSelection, computerSelection) { 
-    if (playerSelection ="rock")  {
-        if (computerSelection = "paper") {
+    if (playerSelection === "rock")  {
+        if (computerSelection === "paper") {
             return ("you loose! paper covers rock");
-        } else if (computerSelection= "scissors") {
+        } else if (computerSelection=== "scissors") {
             return ("Hooray! rock crushes scissors");
-        } else if (computerSelection = "rock") {
+        } else if (computerSelection === "rock") {
             return ("Try again! it's a tie");
         }
-    } else if (playerSelection = "paper") {
-        if (computerSelection = "rock") {
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
             return ("Hooray! paper covers rock");
-        } else if (computerSelection - "scissors") {
+        } else if (computerSelection === "scissors") {
             return ("you loose! Scissors cut paper");
-        } else if (computerSelection = "paper") {
+        } else if (computerSelection === "paper") {
             return ("Try again! it's a tie");
         }
-    }   else if (playerSelection = "scissors") {
-        if (computerSelection = "rock") {
+    }   else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
             return ("you loose! rock crushes scissors");
-        } else if (computerSelection = "paper") {
+        } else if (computerSelection === "paper") {
             return ("Hooray! Scissors cut paper");
-        } else if (computerSelection = "scissors") {
+        } else if (computerSelection === "scissors") {
             return ("Try again! it's a tie");
         }
     } else {
