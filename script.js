d9046bb285346a75a3e536dda97daaf9b3d9bfa3
Avaworld <victoradetunji16@gmail.com>
2022-11-07 11:25:17 +0100

    i played five rounds of game no result saved yet

--- script.js
+++ script.js
@@ -14,64 +14,67 @@
         }
     } return computerChoice;
  } 
-   // console.log(getComputerChoice);//
+   // console.log(getComputerChoice);
 
-// a function that plays a single round of Rock Paper Scissors//
+// a function that plays a single round of Rock Paper Scissors
     var playerSelection = prompt("rock, paper, or scissors?");
-    var playerSelection = playerSelection.toLowerCase (); //i used this to make my input case insensitive//
+    var playerSelection = playerSelection.toLowerCase (); //i used this to make my input case insensitive
     var computerSelection = getComputerChoice ();
+    let playerScore=0;
+    let computerScore=0;
+    let tieScore=0;
     function playRound (playerSelection, computerSelection) { 
     if (playerSelection == "rock")  {
         if (computerSelection == "paper") {
+            computerScore++;
             return ("you loose! paper covers rock");
         } else if (computerSelection== "scissors") {
+            playerScore++;
             return ("Hooray! rock crushes scissors");
         } else if (computerSelection == "rock") {
+            tieScore++;
             return ("Try again! it's a tie");
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
+            playerScore++;
             return ("Hooray! paper covers rock");
         } else if (computerSelection == "scissors") {
+            computerScore++;
             return ("you loose! Scissors cut paper");
         } else if (computerSelection == "paper") {
+            tieScore++;
             return ("Try again! it's a tie");
         }
     }   else if (playerSelection == "scissors") {
         if (computerSelection == "rock") {
+            computerScore++;
             return ("you loose! rock crushes scissors");
         } else if (computerSelection == "paper") {
+            playerScore++;
             return ("Hooray! Scissors cut paper");
         } else if (computerSelection == "scissors") {
+            tieScore++;
             return ("Try again! it's a tie");
         }
     } else {
         return ("there is an unknown error");
     }
-    let ("you loose! paper covers rock") = "you loose";
-    let ("Hooray! rock crushes scissors") = "you win";
-    let ("Hooray! paper covers rock") = "you win";
-    let ("you loose! Scissors cut paper") = "you loose";
-    let ("you loose! rock crushes scissors") = "you loose";
-    let ("Hooray! Scissors cut paper") = "you win";
-    let ("Try again! it/s a tie") = "try again";
-
-    if ("you loose") {
-        console.log("A");
-    } else if ("you win") {
-        console.log("B");
-    } else if ("try again") {
-        console.log("retry");
-    }
-} 
+}
     
- console.log(playRound(playerSelection, computerSelection));
 
  // a function `game` that calls `playRound` and play five rounds of the game while saving results.//
  function game () {
     
 
     for (let i = 0; i < 5; i++) {
-        
-     }
- }
+        var playerSelection = prompt("rock, paper, or scissors?");
+    var playerSelection = playerSelection.toLowerCase (); //i used this to make my input case insensitive
+    var computerSelection = getComputerChoice ();
+    let playerScore=0;
+    let computerScore=0;
+    let tieScore=0;
+        var playGame = playRound(playerSelection, computerSelection);
+        console.log (playGame) ;
+     } 
+ } console.log (game());
